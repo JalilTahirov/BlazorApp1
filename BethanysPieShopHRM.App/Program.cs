dca7a0b4aef606f547653a46a0b77aac0de23cd7
Jalil Tahirov <jalilt@xcritical.software>
2020-11-28 23:16:51 +0400

    Before Form

--- BethanysPieShopHRM.App/Program.cs
+++ BethanysPieShopHRM.App/Program.cs
@@ -7,18 +7,20 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using BethanysPieShopHRM.App.Services;
 
 namespace BethanysPieShopHRM.App
 {
     public class Program
     {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+    public static async Task Main(string[] args)
+    {
+      var builder = WebAssemblyHostBuilder.CreateDefault(args);
+      builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+      //builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+      builder.Services
+        .AddHttpClient<IEmployeeDataService, EmployeeDataService>(client => client.BaseAddress = new Uri("https://localhost:44340"));
             await builder.Build().RunAsync();
         }
     }
