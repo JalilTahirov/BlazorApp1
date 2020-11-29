4709a4b2f7842fafb9685d1f6105355d5b540a0d
Jalil Tahirov <jalilt@xcritical.software>
2020-11-29 10:33:31 +0400

    Adding More Components

--- BethanysPieShopHRM.App/Pages/EmployeeEdit.cs
+++ BethanysPieShopHRM.App/Pages/EmployeeEdit.cs
@@ -13,13 +13,22 @@ namespace BethanysPieShopHRM.App.Pages
     [Inject]
     public IEmployeeDataService EmployeeDataService { get; set; }
 
+    [Inject]
+    public ICountryDataService CountryDataService { get; set; }
+
     [Parameter]
     public string EmployeeId { get; set; }
 
     public Employee Employee { get; set; } = new Employee();
+    public List<Country> Countries { get; set; } = new List<Country>();
+
+    public string CountryId = string.Empty;
+
     protected async override Task OnInitializedAsync()
     {
       Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
+      Countries = (await CountryDataService.GetAllCountries()).ToList();
+      CountryId = Employee.CountryId.ToString(); 
     }
   }
 }
--- BethanysPieShopHRM.App/Program.cs
+++ BethanysPieShopHRM.App/Program.cs
@@ -11,17 +11,24 @@ using BethanysPieShopHRM.App.Services;
 
 namespace BethanysPieShopHRM.App
 {
-    public class Program
-    {
+  public class Program
+  {
     public static async Task Main(string[] args)
     {
       var builder = WebAssemblyHostBuilder.CreateDefault(args);
       builder.RootComponents.Add<App>("app");
 
       //builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
       builder.Services
-        .AddHttpClient<IEmployeeDataService, EmployeeDataService>(client => client.BaseAddress = new Uri("https://localhost:44340"));
-            await builder.Build().RunAsync();
-        }
+       .AddHttpClient<IEmployeeDataService, EmployeeDataService>(client => 
+       client.BaseAddress = new Uri("https://localhost:44340"));
+        await builder.Build().RunAsync();
+
+      builder.Services
+       .AddHttpClient<ICountryDataService, CountryDataService>(
+        client => client.BaseAddress = new Uri("https://localhost:44340"));
+        await builder.Build().RunAsync();
     }
+  }
 }
